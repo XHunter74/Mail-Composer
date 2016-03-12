43b950f3006bd3eca0f2b0b6739c25108f251bac
Sergey Krasovskiy <xhunter74@gmail.com>
2016-03-12 21:11:33 +0200

    Initial commit.

---
+++ app/src/main/java/mailcomposer/xhunter74/com/mailcomposer/MainActivity.java
@@ -0,0 +1,13 @@
+package mailcomposer.xhunter74.com.mailcomposer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
