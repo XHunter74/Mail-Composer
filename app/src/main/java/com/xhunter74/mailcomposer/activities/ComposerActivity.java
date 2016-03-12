37bb3f9f050891fc38eb9fc787381d3b12b1ea73
Sergey Krasovskiy <xhunter74@gmail.com>
2016-03-12 23:46:51 +0200

    Implemented methods for initializing application.

--- app/src/main/java/com/xhunter74/mailcomposer/activities/ComposerActivity.java
+++ app/src/main/java/com/xhunter74/mailcomposer/activities/ComposerActivity.java
@@ -1,15 +1,128 @@
 package com.xhunter74.mailcomposer.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.GmailScopes;
 import com.xhunter74.mailcomposer.R;
 
+import java.util.Arrays;
+
 public class ComposerActivity extends AppCompatActivity {
+
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final int REQUEST_ACCOUNT_PICKER = 1000;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final int REQUEST_AUTHORIZATION = 1001;
+    private static final String[] SCOPES = {GmailScopes.GMAIL_COMPOSE};
+    private GoogleAccountCredential mCredential;
+    private TextView mFromEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composer);
+        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+        prepareControls();
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+    }
+
+    private void prepareControls() {
+        mFromEditText = (TextView) findViewById(R.id.activity_composer_from);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isGooglePlayServicesAvailable()) {
+            initComposer();
+        } else {
+            Toast.makeText(ComposerActivity.this,
+                    getText(R.string.composer_activity_google_play_services_warning),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initComposer() {
+        if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            mFromEditText.setText(mCredential.getSelectedAccountName());
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS) {
+            return false;
+        }
+        return true;
+    }
+
+    private void showGooglePlayServicesAvailabilityErrorDialog(final int connectionStatusCode) {
+        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(connectionStatusCode,
+                ComposerActivity.this, REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    isGooglePlayServicesAvailable();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        mCredential.setSelectedAccountName(accountName);
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(ComposerActivity.this,
+                            getString(R.string.composer_activity_account_unspecified),
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode != RESULT_OK) {
+                    chooseAccount();
+                }
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void chooseAccount() {
+        startActivityForResult(mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
     }
 }
