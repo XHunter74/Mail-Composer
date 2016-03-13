3d591b93c676e2e7895fdff106ca5f312d116f21
Sergey Krasovskiy <xhunter74@gmail.com>
2016-03-13 02:22:37 +0200

    Added model for storing email information.

---
+++ app/src/main/java/com/xhunter74/mailcomposer/models/MessageModel.java
@@ -0,0 +1,52 @@
+package com.xhunter74.mailcomposer.models;
+
+import android.text.TextUtils;
+
+/**
+ * Created by Serhiy.Krasovskyy on 13.03.2016.
+ */
+public class MessageModel {
+    private String mFromAddress;
+    private String[] mRecipientAddresses;
+    private String mSubject;
+    private String mMessageBody;
+
+    public String getFromAddress() {
+        return mFromAddress;
+    }
+
+    public void setFromAddress(String fromAddress) {
+        this.mFromAddress = fromAddress;
+    }
+
+    public String[] getRecipientAddresses() {
+        return mRecipientAddresses;
+    }
+
+    public void setRecipientAddresses(String addresses) {
+        if (!TextUtils.isEmpty(addresses)) {
+            addresses = addresses.replace(',', ';');
+            if (addresses.contains(";")) {
+                mRecipientAddresses = addresses.split(";");
+            } else {
+                mRecipientAddresses = new String[]{addresses};
+            }
+        }
+    }
+
+    public String getSubject() {
+        return mSubject;
+    }
+
+    public void setSubject(String subject) {
+        this.mSubject = subject;
+    }
+
+    public String getMessageBody() {
+        return mMessageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.mMessageBody = messageBody;
+    }
+}
