3fa969c9c662777de2c695a5c07b82fb787a0d48
Serhiy Krasovskyy <xhunter74@gmail.com>
2016-07-06 12:55:28 +0300

    Added equals method.

--- app/src/main/java/com/xhunter74/mailcomposer/models/AttachmentModel.java
+++ app/src/main/java/com/xhunter74/mailcomposer/models/AttachmentModel.java
@@ -19,4 +19,18 @@ public class AttachmentModel {
             return null;
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean result = false;
+        if (o != null && o instanceof AttachmentModel) {
+            AttachmentModel attachment = (AttachmentModel) o;
+            if (!TextUtils.isEmpty(attachment.path) && attachment.path.equals(this.path)) {
+                result = true;
+            } else if (attachment.path == null && this.path == null) {
+                result = true;
+            }
+        }
+        return result;
+    }
 }
